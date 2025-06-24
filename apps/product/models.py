bd653f5fd07a31b10d35163b56b10c22c9ed2e25
silvestre <silvestrevillar02@gmail.com>
2025-06-24 09:01:09 -0300

    feat(models): create model Porduct and serializers

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True,db_column='id_product')
+    name = models.CharField(max_length=50, null=True)
+    stock = models.IntegerField()
+    img = models.CharField(max_length=100,null=True,blank=True)
+    
+    class Meta:
+        db_table = 'Product'
