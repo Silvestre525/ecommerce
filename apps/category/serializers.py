2dadf3d1dc9db38eeb91af5001194ff841ab9dea
silvestre <silvestrevillar02@gmail.com>
2025-07-08 00:23:15 -0300

    feat(serializers): add categorySerilizer

--- apps/category/serializers.py
+++ apps/category/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Category
 
-class categorySerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         field = ['all']
