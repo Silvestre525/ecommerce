ca939d32655b466c090d73ed3f971880d3524d97
silvestre <silvestrevillar02@gmail.com>
2025-07-08 00:43:42 -0300

    feat(urls): add urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/product/', include('apps.product.urls')),
+    path('api/category',include('apps.category.urls'))
 ]
