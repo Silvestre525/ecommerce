dd98963d944387a4f8dbbf2a8f2aa961d0b5320f
silvestre <silvestrevillar02@gmail.com>
2025-07-24 22:04:50 -0300

    modify(suppliers/models): rename blamk to blank)_

--- apps/suppliers/models.py
+++ apps/suppliers/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Suppliers(models.Model):
-    id = models.Autofield(primary_key=True)
+    id = models.AutoField(primary_key=True)
     comany_name = models.CharField(max_length=50, null=False, blank=False)
     contact_person= models.CharField(max_length=50, null=False, blank=False)
-    contact_email = models.EmailField(max_length=200, null=False, blamk=False)
+    contact_email = models.EmailField(max_length=200, null=False, blank=False)
     adress = models.CharField(max_length=100, null=False, blank=False)
     Country = models.CharField(max_length=50,null=False, blank=False)
