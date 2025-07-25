35970af2c65bf5941d78fbfe25abd7f68c2bb0d8
silvestre <silvestrevillar02@gmail.com>
2025-07-24 23:08:42 -0300

    modify

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from ..category.models import Category
-
+from ..suppliers.models import Suppliers
 # Create your models here.
 class Product(models.Model):
     id = models.AutoField(primary_key=True,db_column='id_product')
@@ -9,6 +9,8 @@ class Product(models.Model):
     img = models.CharField(max_length=100,null=True,blank=True)
 
     categories = models.ManyToManyField(Category, related_name='products')
+
+    product = models.ForeignKey(Suppliers, related_name="suppliers", on_delete=models.CASCADE) 
     
     class Meta:
         db_table = 'Product'
--- apps/suppliers/models.py
+++ apps/suppliers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Suppliers(models.Model):
     id = models.AutoField(primary_key=True)
     comany_name = models.CharField(max_length=50, null=False, blank=False)
@@ -7,3 +8,8 @@ class Suppliers(models.Model):
     contact_email = models.EmailField(max_length=200, null=False, blank=False)
     adress = models.CharField(max_length=100, null=False, blank=False)
     Country = models.CharField(max_length=50,null=False, blank=False)
+
+    class Meta:
+        db_table = "Suppliers"
+
+    
