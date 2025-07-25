7502e2a55a80e48281e8e4172aeb14e29ed0274c
silvestre <silvestrevillar02@gmail.com>
2025-07-24 22:09:47 -0300

    feat(suppliers/views): add view

--- apps/suppliers/views.py
+++ apps/suppliers/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import models
+from .models import Suppliers
+from .serializers import suppliersSerializer
 
-# Create your views here.
+class suppliersViewSet(models.ModelViewSet):
+    queryset = Suppliers.object.all()
+    serializer_class = suppliersSerializer
