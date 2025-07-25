0a4e5bcc640f8fba726b99740c40285dcf055c72
silvestre <silvestrevillar02@gmail.com>
2025-07-24 21:58:39 -0300

    feat(suppliers/models): add model

--- apps/suppliers/models.py
+++ apps/suppliers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Suppliers(models.Model):
+    id = models.Autofield(primary_key=True)
+    comany_name = models.CharField(max_length=50, null=False, blank=False)
+    contact_person= models.CharField(max_length=50, null=False, blank=False)
+    contact_email = models.EmailField(max_length=200, null=False, blamk=False)
+    adress = models.CharField(max_length=100, null=False, blank=False)
+    Country = models.CharField(max_length=50,null=False, blank=False)
