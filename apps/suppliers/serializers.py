2dad5e5344e0b70983fca7940df9ff79a98c7ad1
silvestre <silvestrevillar02@gmail.com>
2025-07-24 22:04:07 -0300

    feat(suppliers/serializers) add serializers

---
+++ apps/suppliers/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Suppliers
+
+
+class suppliersSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Suppliers
+        fields = ['all']
