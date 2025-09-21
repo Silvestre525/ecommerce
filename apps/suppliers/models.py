151e1468afeab52f470614c4cad0dcaae48097ee
silvestre <silvestrevillar02@gmail.com>
2025-09-20 23:32:28 -0300

    remove propertie country

--- apps/suppliers/models.py
+++ apps/suppliers/models.py
@@ -7,7 +7,7 @@ class Suppliers(BaseModel):
     contact_person= models.CharField(max_length=50, null=False, blank=False)
     contact_email = models.EmailField(max_length=200, null=False, blank=False)
     adress = models.CharField(max_length=100, null=False, blank=False)
-    Country = models.CharField(max_length=50,null=False, blank=False)
+  
 
     class Meta:
         db_table = "Suppliers"
