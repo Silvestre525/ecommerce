dcf9ae14c6ad8e7a369dabf65f80bf7d8abb73d8
silvestre <silvestrevillar02@gmail.com>
2025-09-21 00:28:15 -0300

    modify(suppliers/models): add foreigKey and modify property

--- apps/suppliers/models.py
+++ apps/suppliers/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 from ..BaseModel.models import BaseModel
-
+from ..geo.models import Country, Province, City
 class Suppliers(BaseModel):
     id = models.AutoField(primary_key=True)
-    comany_name = models.CharField(max_length=50, null=False, blank=False)
+    company_name = models.CharField(max_length=50, null=False, blank=False)
     contact_person= models.CharField(max_length=50, null=False, blank=False)
     contact_email = models.EmailField(max_length=200, null=False, blank=False)
     adress = models.CharField(max_length=100, null=False, blank=False)
+    
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='suppliers')
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name='suppliers')
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='suppliers')
   
 
     class Meta:
         db_table = "Suppliers"
 
+    def __str__(self):
+        return self.company_name
     
