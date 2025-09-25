5bc3c456ae05a13c4a59f9a8a394e2e1c37e7e2c
silvestre <silvestrevillar02@gmail.com>
2025-09-25 18:26:23 -0300

    feat(product/view): add admin permissions

--- apps/product/views.py
+++ apps/product/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from utils.permissions import IsAdministrador
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'destroy']:
+            permission_classes = [IsAdministrador]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [p() for p in permission_classes]
