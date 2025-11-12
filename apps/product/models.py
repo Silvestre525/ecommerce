864c02aae8ea62dbe750c37c1d38e173d39b669a
silvestre <silvestrevillar02@gmail.com>
2025-11-12 12:54:38 -0300

    add relation in producto with color and size@

--- apps/product/models.py
+++ apps/product/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from ..category.models import Category
 from ..suppliers.models import Suppliers
 from ..order.models import Order
+from ..color.models import Color
+from ..size.models import Size
 from ..BaseModel.models import BaseModel
 # Create your models here.
 class Product(BaseModel):
@@ -15,6 +17,9 @@ class Product(BaseModel):
     suppliers = models.ManyToManyField(Suppliers, related_name='products')
     
     order = models.ManyToManyField(Order,related_name='products')
+
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, default=1)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, default=1)
     
     class Meta:
         db_table = 'Product'
