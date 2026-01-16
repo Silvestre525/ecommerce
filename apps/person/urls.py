324b8e2576f2b13a05a9c3b241fd0610277a3b1c
silvestre <silvestrevillar02@gmail.com>
2026-01-16 12:26:34 -0300

    modify(person/urls): modify urls.py

--- apps/person/urls.py
+++ apps/person/urls.py
@@ -3,7 +3,7 @@ from .views import RegisterView, ProfileView
 from rest_framework.authtoken import views as authtoken_views
 
 urlpatterns = [
-    path('auth/register/', RegisterView.as_view(), name='register'),
-    path('auth/login/', authtoken_views.obtain_auth_token, name='login'),
-    path('auth/profile/', ProfileView.as_view(), name='profile'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', authtoken_views.obtain_auth_token, name='login'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
